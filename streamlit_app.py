830ba32df212234e92cfac952b9cde1eb6838181
Ahmet Ertok <57173325+ahmetertok@users.noreply.github.com>
2022-08-08 14:35:08 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,11 @@ th = t.hour
 
 def predict(dt,th):
   x = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=40.988925&lon=29.038308&appid=53609485ee8d32929ce452d8cdb1f82e&units=metric')
-  
+  x2 = requests.get('https://api.ibb.gov.tr/tkmservices/api/TrafficData/v1/TrafficIndexHistory/1/H')
+  y2 = json.loads(x2.text)
   y = json.loads(x.text)
   X_predict = pd.DataFrame()
-  dict = {'fusedhour':th,  'weekDay': dt, 'AVERAGE_SPEED':50, 'temp':float(y["main"]['temp']), 'rain':0, 'rain,snow':0, 'snow':0}
+  dict = {'fusedhour':th,  'weekDay': dt, 'AVERAGE_SPEED':y2[0]['TrafficIndex'], 'temp':float(y["main"]['temp']), 'rain':0, 'rain,snow':0, 'snow':0}
   if str(y["weather"][0]['id'])[0:1]=='6':
     dict['snow']=1
   if str(y["weather"][0]['id'])[0:1]=='5':
@@ -38,12 +39,11 @@ def predict(dt,th):
   X_predict= X_predict.append(dict, ignore_index = True)
   prediction = loaded_model.predict(X_predict)
   return int(prediction)
-x2 = requests.get('https://api.ibb.gov.tr/tkmservices/api/TrafficData/v1/TrafficIndexHistory/1/H')
-y2 = json.loads(x2.text)
+
 output = predict(dt,th)
 st.title("Metro vardığınızda %"+ str(int((int(output)*100)/15000)) +" yoğun olacak.")
 st.write("Seçilen tarihte ~"+str(output)+" yolcu var.")
-st.write(y2[0]['TrafficIndex'])
+#st.write(y2[0]['TrafficIndex'])
 #a = json.loads(str(y["weather"])[1:len(str(y["weather"]))-1])
 #st.title(str(y["weather"][0]['id'])[0:1])
 
