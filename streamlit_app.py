15757c6f00eb35349cc28444ff603a158d75f187
Ahmet Ertok <57173325+ahmetertok@users.noreply.github.com>
2022-08-08 14:34:19 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,11 @@ def predict(dt,th):
   prediction = loaded_model.predict(X_predict)
   return int(prediction)
 x2 = requests.get('https://api.ibb.gov.tr/tkmservices/api/TrafficData/v1/TrafficIndexHistory/1/H')
+y2 = json.loads(x2.text)
 output = predict(dt,th)
 st.title("Metro vardığınızda %"+ str(int((int(output)*100)/15000)) +" yoğun olacak.")
 st.write("Seçilen tarihte ~"+str(output)+" yolcu var.")
-st.write(x2.text)
+st.write(y2[0]['TrafficIndex'])
 #a = json.loads(str(y["weather"])[1:len(str(y["weather"]))-1])
 #st.title(str(y["weather"][0]['id'])[0:1])
 
